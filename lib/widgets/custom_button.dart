916481ab3f8387440ec6ed0accd2d746083f187c
24shrouk <shroukhegab3@gmail.com>
2024-02-15 17:12:39 +0200

    finish notes bottom sheet

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constant.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
